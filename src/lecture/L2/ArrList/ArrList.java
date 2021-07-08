1038903cc22662d17f8c269d86fc3e5b2b6ea903
gabrielyoon7 <gabrielyoon7@gmail.com>
2021-07-09 03:44:09 +0900

    .

--- src/lecture/L2/ArrList/ArrList.java
+++ src/lecture/L2/ArrList/ArrList.java
@@ -41,6 +41,7 @@ public class ArrList <E> {
 	}
 
 	private void resize(int newSize) {		// 배열 크기 조절
+		System.out.println("resized");
 		Object[] t = new Object[newSize];   // newSize 크기의 새로운 배열 t 생성
 		for (int i = 0; i < size; i++)
 			t[i] = a[i];                    // 배열 s를 배열 t로 복사
